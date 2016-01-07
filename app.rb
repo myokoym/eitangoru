1d32eae5825770b8b6aa9a39cca13254978c8f54
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-01-08 00:37:10 +0900

    Import

---
+++ app.rb
@@ -0,0 +1,16 @@
+require "sinatra/base"
+require "active_record"
+require "haml"
+
+ActiveRecord::Base.configurations = YAML.load_file("database.yml")
+ActiveRecord::Base.establish_connection(:development)
+
+class Word < ActiveRecord::Base
+end
+
+class App < Sinatra::Base
+  get "/" do
+    @word = Word.offset(rand(Word.count)).first
+    haml :index
+  end
+end
