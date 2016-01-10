008cb7f02a2ff4d58ddbc9ec6db639ec086838f3
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-01-10 20:38:48 +0900

    Use id instead of class

--- assets/js/application.js
+++ assets/js/application.js
@@ -6,15 +6,15 @@ var load = function() {
     url: "/word.json",
     dataType: "json"
   }).done(function(json) {
-    $("h2.word").text(json.word);
-    $("p.lead").text(json.mean);
+    $("#word").text(json.word);
+    $("#mean").text(json.mean);
     $("#frequency").text(json.level);
   });
 }
 
 $(document).ready(function() {
   load();
-  $(".clickableBox").click(function() {
+  $("#clickableBox").click(function() {
     load();
   });
 });
